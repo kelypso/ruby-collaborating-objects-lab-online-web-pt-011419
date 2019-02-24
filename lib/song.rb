642fa7f9ac9988ece36c496446a13927e93efd17
Kelsey Low <kklowlow@gmail.com>
2019-02-24 19:03:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     song = filename.split(" - ")[1] # remove .mp3 and separate song name 
     artist = filename.split(" - ")[0] # separate artist name
     new_song = self.new(song) # create a new instance of this song
-    new_song.artist_name = artist # set this song's artist to the artist
+    new_song.artist_name = artist # set this song's artist to the artist using artist=
     new_song
   end
   
@@ -19,23 +19,3 @@ class Song
     artist.add_song(self) # adds song instance to array in artist instance
   end
 end
-
-
-
-=begin
-
-  def self.new_by_filename(file_name)
-    song = file_name.split(" - ")[1]
-    artist = file_name.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
-  end
-
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
-end
-
-=end
