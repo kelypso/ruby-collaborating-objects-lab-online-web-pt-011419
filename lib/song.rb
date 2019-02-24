7b622dd9041d54e3fdb0a6e6ea87460906ae8aeb
Kelsey Low <kklowlow@gmail.com>
2019-02-24 17:56:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
     # takes song artist string and finds/creates artist object
     self.artist = Artist.find_or_create_by_name(artist)
     # adds song instance to array in artist instance
-    atist.add_song(self) 
+    artist.add_song(self
   end
 end
 
